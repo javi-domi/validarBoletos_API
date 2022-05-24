b209a303eebfa7df8911d36646309ac7b94f04f9
javi-domi <javdominguez@icloud.com>
2022-05-24 19:50:04 -0300

    [test]: implement test for business logic

--- src/boleto/boleto.service.spec.ts
+++ src/boleto/boleto.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoletoService } from './boleto.service';
 
-describe('ValidateBoletoService', () => {
+describe('Validate Boleto Service', () => {
   let service: BoletoService;
 
   beforeEach(async () => {
@@ -12,7 +12,25 @@ describe('ValidateBoletoService', () => {
     service = module.get<BoletoService>(BoletoService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Should execute bank flow if the code have 47 digits', () => {
+    const boletoBancario = jest.spyOn(service, 'boletoBancario');
+    const concessionaria = jest.spyOn(service, 'concessionaria');
+    const result = service.execute(
+      '21290001192110001210904475617405975870000002000',
+    );
+    expect(result.barCode.length).toBe(44);
+    expect(boletoBancario).toHaveBeenCalledTimes(1);
+    expect(concessionaria).toHaveBeenCalledTimes(0);
+  });
+
+  it('Should execute concessionary flow if the code have 48 digits', () => {
+    const boletoBancario = jest.spyOn(service, 'boletoBancario');
+    const concessionaria = jest.spyOn(service, 'concessionaria');
+    const result = service.execute(
+      '836300000053157800863019653978624019100220176190',
+    );
+    expect(result.barCode.length).toBe(44);
+    expect(boletoBancario).toHaveBeenCalledTimes(0);
+    expect(concessionaria).toHaveBeenCalledTimes(1);
   });
 });
