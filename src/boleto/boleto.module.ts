e8ebeeb9b052fc97667c8ece5787f060cf3dcf0c
javi-domi <javdominguez@icloud.com>
2022-05-24 19:48:29 -0300

    feat: import helpers into module context

--- src/boleto/boleto.module.ts
+++ src/boleto/boleto.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BoletoHelpers } from 'src/helpers/helpers';
 import { BoletoController } from './boleto.controller';
 import { BoletoService } from './boleto.service';
 
 @Module({
+  imports: [BoletoHelpers],
   controllers: [BoletoController],
   providers: [BoletoService],
 })
